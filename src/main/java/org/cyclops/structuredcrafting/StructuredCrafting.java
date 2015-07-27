7bb667520da1626d3e6f622d8dee6161b0d1f196
Ruben Taelman <rubensworks@gmail.com>
2015-07-27 10:41:17 +0200

    Use base mod class for versions

--- src/main/java/org/cyclops/structuredcrafting/StructuredCrafting.java
+++ src/main/java/org/cyclops/structuredcrafting/StructuredCrafting.java
@@ -7,10 +7,9 @@ import org.apache.logging.log4j.Level;
 import org.cyclops.cyclopscore.config.ConfigHandler;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockItemConfigReference;
 import org.cyclops.cyclopscore.init.ItemCreativeTab;
-import org.cyclops.cyclopscore.init.ModBase;
+import org.cyclops.cyclopscore.init.ModBaseVersionable;
 import org.cyclops.cyclopscore.init.RecipeHandler;
 import org.cyclops.cyclopscore.proxy.ICommonProxy;
-import org.cyclops.cyclopscore.tracking.IModVersion;
 import org.cyclops.structuredcrafting.block.BlockStructuredCrafterConfig;
 import org.cyclops.structuredcrafting.craft.provider.IItemStackProviderRegistry;
 import org.cyclops.structuredcrafting.craft.provider.InventoryItemStackProvider;
@@ -29,7 +28,7 @@ import org.cyclops.structuredcrafting.craft.provider.WorldItemStackProvider;
      dependencies = Reference.MOD_DEPENDENCIES,
      guiFactory = "org.cyclops.structuredcrafting.GuiConfigOverview$ExtendedConfigGuiFactory"
 )
-public class StructuredCrafting extends ModBase implements IModVersion {
+public class StructuredCrafting extends ModBaseVersionable {
 
     /**
      * The unique instance of this mod.
@@ -37,14 +36,8 @@ public class StructuredCrafting extends ModBase implements IModVersion {
     @Mod.Instance(value = Reference.MOD_ID)
     public static StructuredCrafting _instance;
 
-    private boolean versionInfo = false;
-    private String version;
-    private String info;
-    private String updateUrl;
-
     public StructuredCrafting() {
-        super(Reference.MOD_ID, Reference.MOD_NAME);
-        putGenericReference(REFKEY_MOD_VERSION, Reference.MOD_VERSION);
+        super(Reference.MOD_ID, Reference.MOD_NAME, Reference.MOD_VERSION);
     }
 
     @Override
@@ -106,39 +99,6 @@ public class StructuredCrafting extends ModBase implements IModVersion {
     @Override
     public ICommonProxy getProxy() {
         return null;
-    }
-
-    @Override
-    public void setVersionInfo(String version, String info, String updateUrl) {
-        versionInfo = true;
-        this.version = version;
-        this.info = info;
-        this.updateUrl = updateUrl;
-    }
-
-    @Override
-    public boolean isVersionInfo() {
-        return versionInfo;
-    }
-
-    @Override
-    public String getVersion() {
-        return version;
-    }
-
-    @Override
-    public String getInfo() {
-        return info;
-    }
-
-    @Override
-    public String getUpdateUrl() {
-        return updateUrl;
-    }
-
-    @Override
-    public boolean needsUpdate() {
-        return getVersion() != null && !Reference.MOD_VERSION.equals(getVersion());
     }
 
     /**
