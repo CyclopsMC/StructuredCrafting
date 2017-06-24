bf5e5465c85a5b529ef16a41cfd8857c29d290cb
Ruben Taelman <rubensworks@gmail.com>
2017-06-24 14:25:21 +0200

    Bump Forge to 14.21.0.2359

--- src/main/java/org/cyclops/structuredcrafting/Reference.java
+++ src/main/java/org/cyclops/structuredcrafting/Reference.java
@@ -19,7 +19,7 @@ public final class Reference {
     // MOD ID's
     public static final String MOD_FORGE = "forge";
     public static final String MOD_FORGE_VERSION = "@FORGE_VERSION@";
-    public static final String MOD_FORGE_VERSION_MIN = "14.21.0.2343";
+    public static final String MOD_FORGE_VERSION_MIN = "14.21.0.2359";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_MINVERSION = "@CYCLOPSCORE_VERSION@";
     public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.6";
