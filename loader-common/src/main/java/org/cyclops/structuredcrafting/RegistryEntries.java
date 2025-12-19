92e95dd863801507a74983fe4baa20f3bd554981
Ruben Taelman <rubensworks@gmail.com>
2025-12-19 11:34:37 +0100

    Update to MC 1.21.11

--- loader-common/src/main/java/org/cyclops/structuredcrafting/RegistryEntries.java
+++ loader-common/src/main/java/org/cyclops/structuredcrafting/RegistryEntries.java
@@ -1,7 +1,7 @@
 package org.cyclops.structuredcrafting;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -14,10 +14,10 @@ import org.cyclops.structuredcrafting.blockentity.BlockEntityStructuredCrafter;
  */
 public class RegistryEntries {
 
-    public static final DeferredHolderCommon<Item, Item> ITEM_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.ITEM, ResourceLocation.parse("structuredcrafting:structured_crafter"));
+    public static final DeferredHolderCommon<Item, Item> ITEM_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.ITEM, Identifier.parse("structuredcrafting:structured_crafter"));
 
-    public static final DeferredHolderCommon<Block, Block> BLOCK_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.BLOCK, ResourceLocation.parse("structuredcrafting:structured_crafter"));
+    public static final DeferredHolderCommon<Block, Block> BLOCK_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.BLOCK, Identifier.parse("structuredcrafting:structured_crafter"));
 
-    public static final DeferredHolderCommon<BlockEntityType<?>, BlockEntityType<BlockEntityStructuredCrafter>> BLOCK_ENTITY_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.BLOCK_ENTITY_TYPE, ResourceLocation.parse("structuredcrafting:structured_crafter"));
+    public static final DeferredHolderCommon<BlockEntityType<?>, BlockEntityType<BlockEntityStructuredCrafter>> BLOCK_ENTITY_STRUCTURED_CRAFTER = DeferredHolderCommon.create(Registries.BLOCK_ENTITY_TYPE, Identifier.parse("structuredcrafting:structured_crafter"));
 
 }
